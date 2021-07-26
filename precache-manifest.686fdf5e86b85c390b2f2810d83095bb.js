e590793e19745b1033f5f7012584c27a3c05d5f9
Oleg.Z <gozlotgo@gmail.com>
2021-07-26 23:56:28 +0300

    Updates

---
+++ precache-manifest.686fdf5e86b85c390b2f2810d83095bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df83d029e66d33e82852e3833120968",
+    "url": "/mini-tasks/index.html"
+  },
+  {
+    "revision": "5051a6eb3bf444c69920",
+    "url": "/mini-tasks/static/css/main.93d5290c.chunk.css"
+  },
+  {
+    "revision": "d9238acbfb46618858e0",
+    "url": "/mini-tasks/static/js/2.6166395c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mini-tasks/static/js/2.6166395c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5051a6eb3bf444c69920",
+    "url": "/mini-tasks/static/js/main.4d274208.chunk.js"
+  },
+  {
+    "revision": "ecb8a7f92d427f9c455c",
+    "url": "/mini-tasks/static/js/runtime-main.05ecae78.js"
+  }
+]);
